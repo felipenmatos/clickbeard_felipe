1d9484fc81221b353f15f3d458450a2579ab766b
Felipe Carvalho Nonato Matos <felipe.matos@XDNTRAVEL.CORP>
2022-03-14 02:24:06 -0300

    style: delete button styling

--- src/Componentes/ConfirmDelete/Delete.js
+++ src/Componentes/ConfirmDelete/Delete.js
@@ -21,25 +21,58 @@ function ModalDelete({show, setClose, message, handleConfirm }) {
 }
 
 const Container = styled.div`
-    width: 80px;
-    margin-right: -80px;
-    background: red;
+    width: 110px;
+    height: 70px;
+    margin-top: 115px;
+    margin-left: -40px;
+    margin-right: -85px;
+    padding: 7px;
+    background: linear-gradient(91.26deg, #404040 0%, #707070 97.77%);
+    border-radius: 4px;
+    z-index: 1;
 `;
 
 const Div = styled.div`
-
+    position: absolute;
+    top: -10px;
+    width: 0;
+    height: 0;
+    border-left: 8px solid trasparent;
+    border-right: 8px solid trasparent;
+    border-bottom: 10px solid #e4f2fd;    
 `;
 
 const Span = styled.span`
-
+    display: block;
+    font-size: 15px;
+    font-family: 'Ubuntu', sans-serif;
+    color: #FFF;
 `;
 
 const ButtonConfirm = styled.button`
+    margin: 5px;
+    padding: 10px;
+    width: 45px;
+    border: none;
+    border-radius: 4px;
+    font-size: 14px;
 
+    :hover{
+        opacity: 0.8;
+    }
 `;
 
 const ButtonNo = styled.button`
+    margin: 5px;
+    padding: 10px;
+    width: 45px;
+    border: none;
+    border-radius: 4px;
+    font-size: 14px;
 
+    :hover{
+        opacity: 0.8;
+    }
 `;
 
 export default ModalDelete;
